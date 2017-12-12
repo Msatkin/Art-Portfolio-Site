ec83d3a5593e052d600fee9fd995b718de2e8fb2
Msatkin <mscottaa@gmail.com>
2017-12-12 20:09:00 +0100

    Added contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './index.css';
+import Background from '../../components/background';
+
 
 const Contact = (props) => {
     return (
         <div className="Contact">
-            <h1>Contact</h1>
+            <Background src="/contactBackground.jpg" alt="Blurry background"/>
+            <div className="contact-container">
+                <div className="contact-box">
+                    <div className="contact-title">
+                        <h1 className="">Get in contact!</h1>
+                    </div>
+                    <div className="contact-icons">
+                        <div className="contact-icon-container">
+                            <i className="fa fa-envelope fa-5x email-icon" aria-hidden="true"></i>
+                            <p>Email</p>
+                        </div>
+                        <div className="contact-icon-container">
+                            <i className="fa fa-instagram fa-5x instagram-icon" aria-hidden="true"></i>
+                            <p>Instagram</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
